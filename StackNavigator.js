fa8bac93f498bf329a2ed3e275459e8dffc8fd93
Andile <andile.goba@younglings.africa>
2022-11-17 15:24:17 +0200

    first commit

---
+++ StackNavigator.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './screens/HomeScreen';
+import QuizScreen from './screens/QuizScreen';
+import ResultsScreen from './screens/ResultsScreen';
+
+const StackNavigator = () => {
+    const Stack = createNativeStackNavigator();
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="Quiz" component={QuizScreen} options={{ headerShow: false }} />
+                <Stack.Screen name="Results" component={ResultsScreen} options={{ headerShow: false }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default StackNavigator;
+
+const styles = StyleSheet.create({});
---
+++ data/question.js
@@ -0,0 +1,133 @@
+export default [
+    {
+        question: "How sexy, is Sexy Ace?",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"Sexy",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"Very Sexy",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"Extremely Sexy",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"Yogurt",
+            },
+        ],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "How many six packs does Sexy Ace have?",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"8",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"3",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"6",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"1",
+            },
+        ],
+        correctAnswerIndex: 0,
+
+    },
+    {
+        question: "What is Sexy Ace good at?",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"Being Sexy",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"Being too damn sexy",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"Saying Aweh!",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"Nommer maak voll",
+            },
+        ],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "what is 1 + 1 = ?, in Sexy Ace's terms.",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"Us",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"Pure maths",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"Me and U",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"2",
+            },
+        ],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "What is Sexy Ace's dads name?",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"Dwayne The Rock Johnson",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"Vin Diesel",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"Dwayne The Rock Johnson & Vin Diesel",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"Madonna",
+            },
+        ],
+        correctAnswerIndex: 2
+    }
+]
